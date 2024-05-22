bbc8ebdc0d9d38b2417a0e6e86e1db6ea5c7d457
sajalsaraf <sajalsaraf161203@gmail.com>
2024-05-22 19:25:27 +0530

    learning rootquery

---
+++ schema/schema.js
@@ -0,0 +1,39 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema
+} = graphql;
+
+const users = [
+    {id:'23',firstName:"Sajal",age:20},
+    {id:'47',firstName:"Shivam",age:25}
+]
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+          return _.find(users,{id:args.id});
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');// it is a package present in node modules extract using require
 const expressGraphQL = require('express-graphql').graphqlHTTP
 
+const schema = require('./schema/schema')
+
 const app = express();
 
 app.use('/graphql',expressGraphQL({
+    schema, // resembles schema:schema
     graphiql:true
 }))
 
