0792603501f13729729f3ceaa7938151ec8f0820
sajalsaraf <sajalsaraf161203@gmail.com>
2024-05-23 19:44:29 +0530

    from graphiql to to graphql and then get data from different api

--- schema/schema.js
+++ schema/schema.js
@@ -1,16 +1,12 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
+
 const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
   GraphQLSchema
 } = graphql;
-
-const users = [
-    {id:'23',firstName:"Sajal",age:20},
-    {id:'47',firstName:"Shivam",age:25}
-]
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -28,7 +24,9 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-          return _.find(users,{id:args.id});
+          return axios.get(`http://localhost:3000/users/${args.id}`)
+          .then(resp => resp.data);
+          
       }
     }
   }
@@ -37,3 +35,11 @@ const RootQuery = new GraphQLObjectType({
 module.exports = new GraphQLSchema({
     query: RootQuery
 })
+
+//
+
+// {
+//   user(id:"40"){
+//     firstName
+//   }
+// }
